0d2aadfc45213a08865305a32c5b5026bb5beeeb
PaulBuske <paul.a.buske.mil@swf.army.mil>
2021-09-23 13:38:57 -0500

    Initial Commit for Endpoints Exercise

---
+++ src/main/java/com/galvanize/Spring/Lessons/CRUD/Endpoints/SpringLessonsCrudEndpointsApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.Spring.Lessons.CRUD.Endpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLessonsCrudEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLessonsCrudEndpointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/Spring/Lessons/CRUD/Endpoints/SpringLessonsCrudEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.Spring.Lessons.CRUD.Endpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLessonsCrudEndpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
